9a30317ad9960e9690b70795c384d5064a712b06
JoaoLucasRch <joaolucascroch@gmail.com>
2025-06-18 17:07:42 -0300

    questão 1 correção

--- questão1/app.js
+++ questão1/app.js
@@ -1,4 +1,4 @@
 const { caculateAge } = require('./modulo');
 
-const dataNascimento = '2005-06-18';
-console.log(`Sua idade é: ${caculateAge(dataNascimento)} anos`);
+const dt = '2005-06-18';
+console.log(`Sua idade é: ${caculateAge(dt)} anos`);
